094eb22974f18614ad2121425f54cb76a9d9af71
sarasandager <sara590x@edu.easj.dk>
2021-02-23 23:13:37 +0100

    Solved Milestone 2: UI-elements and basic functionality added.
    onClick handler for enterButton solved.

--- app/src/main/java/com/example/cleanarchitecturedemo/MainActivity.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.cleanarchitecturedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button enterButton;
+    TextView outputView;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterButton = findViewById(R.id.enterButton);
+        outputView = findViewById(R.id.outputView);
+        inputText = findViewById(R.id.inputText);
+
+        enterButton.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String edit = inputText.getEditableText().toString();
+        outputView.setText(edit);
     }
 }
